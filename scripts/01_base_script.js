7b6cacefa533856e49f3ba819de1bd8e5326dbc5
mzyrkowski <marcinzyrkowski7@gmail.com>
2024-08-19 14:47:51 +0200

    refactoring

--- scripts/01_base_script.js
+++ scripts/01_base_script.js
@@ -1,18 +1,16 @@
 // script code should be in arrow function
 const {chromium} = require('playwright');
 
-( async () => { 
+(async () => {
+  // by default playwright works in headless mode, if we want to turn it off, we need to set flag headless to false
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-    // by default playwright works in headless mode, if we want to turn it off, we need to set flag headless to false
-    const browser = await chromium.launch({headless:false})
-    const context = await browser.newContext()
-    const page = await context.newPage()
+  // we can instantiate page in this way. but only for single page and simple scripts
+  // const page = browser.newPage()
 
-    // we can instantiate page in this way. but only for single page and simple scripts
-    // const page = browser.newPage()
-
-    await page.goto('https://todomvc.com/examples/react/dist/')
-    await page.screenshot({path:`resources/01_screenshot.png`})
-    await browser.close()
-
-}) ()
+  await page.goto("https://todomvc.com/examples/react/dist/");
+  await page.screenshot({ path: `resources/01_screenshot.png` });
+  await browser.close();
+})();
